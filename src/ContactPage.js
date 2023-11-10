c88a9b7e35df83685be145c09a29945a86605958
Willie <williekaufman1@gmail.com>
2023-11-09 20:19:46 -0500

    x

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import Typography from "@material-ui/core/Typography";
 
 export default function ContactPage() {
+  let navigate = useNavigate();
+  let oldTitle = document.title;
+
+  useEffect(() => {
+    document.title = "Contact";
+
+    function handleKeyDown(event) {
+      if (event.key === "Backspace") {
+        navigate("/");
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      document.title = oldTitle;
+    };
+  }, []);
+
   return (
     <div
       className="page-container"
       style={{ textAlign: "center", color: "white" }}
     >
       <Typography variant="h2">
-        Please send us an email at gggggopher@gmail.com.
+        Send us an email at gggggopher@gmail.com.
       </Typography>
+      <br />
+      <Link to="/" style={{ color: "white" }}>
+        <Typography variant="h3">Back</Typography>
+      </Link>
     </div>
   );
 }
