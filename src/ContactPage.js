fe8f30ebc18d74c603256b2adf69ee0f1e242a58
Adam Yedidia <adamyedidia@gmail.com>
2023-11-09 19:43:09 -0500

    RIP exclam

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import Typography from '@material-ui/core/Typography';
+import Typography from "@material-ui/core/Typography";
 
 export default function ContactPage() {
-    return (
-        <div className="page-container" style={{ textAlign: 'center', color: 'white' }}>
-            <Typography variant="h2">
-                Please send us an email at gggggopher@gmail.com!
-            </Typography>
-        </div>
-    )
+  return (
+    <div
+      className="page-container"
+      style={{ textAlign: "center", color: "white" }}
+    >
+      <Typography variant="h2">
+        Please send us an email at gggggopher@gmail.com.
+      </Typography>
+    </div>
+  );
 }
